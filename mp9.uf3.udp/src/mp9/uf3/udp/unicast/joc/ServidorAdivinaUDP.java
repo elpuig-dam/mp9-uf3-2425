b7e9a8d9eaec554767c12f3c9fb94dde7aeeb288
jherna <jherna76@elpuig.xeill.net>
2024-11-26 15:12:12 +0100

    add joc udp adivina

--- mp9.uf3.udp/src/mp9/uf3/udp/unicast/joc/ServidorAdivinaUDP.java
+++ mp9.uf3.udp/src/mp9/uf3/udp/unicast/joc/ServidorAdivinaUDP.java
@@ -23,7 +23,74 @@ import java.nio.ByteBuffer;
 
 public class ServidorAdivinaUDP {
 
-	
 
+    DatagramSocket socket;
+    int port, fi;
+    SecretNum ns;
+    boolean acabat;
+
+    public ServidorAdivinaUDP(int port, int max) {
+        try {
+            socket = new DatagramSocket(port);
+            System.out.printf("Servidor obert pel port %d%n",port);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        this.port = port;
+        ns = new SecretNum(max);
+        acabat = false;
+        fi=-1;
+    }
+
+    public void runServer() throws IOException{
+        byte [] receivingData = new byte[4];
+        byte [] sendingData;
+        InetAddress clientIP;
+        int clientPort;
+
+        //el servidor atén el port indefinidament
+        while(!acabat){
+
+            //creació del paquet per rebre les dades
+            DatagramPacket packet = new DatagramPacket(receivingData, 4);
+            //espera de les dades
+            socket.receive(packet);
+            //processament de les dades rebudes i obtenció de la resposta
+            sendingData = processData(packet.getData(), packet.getLength());
+            //obtenció de l'adreça del client
+            clientIP = packet.getAddress();
+            //obtenció del port del client
+            clientPort = packet.getPort();
+            //creació del paquet per enviar la resposta
+            packet = new DatagramPacket(sendingData, sendingData.length,
+                    clientIP, clientPort);
+            //enviament de la resposta
+            socket.send(packet);
+        }
+        socket.close();
+    }
+
+    private byte[] processData(byte[] data, int length) {
+        int nombre = ByteBuffer.wrap(data).getInt();
+        System.out.println("rebut->"+nombre);
+        fi = ns.comprova(nombre);
+        if(fi==0) acabat=true;
+        byte[] resposta = ByteBuffer.allocate(4).putInt(fi).array();
+        return resposta;
+    }
+
+    public static void main(String[] args) throws SocketException, IOException {
+        ServidorAdivinaUDP sAdivina = new ServidorAdivinaUDP(5556, 100);
+
+        try {
+            sAdivina.runServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Fi Servidor");
+
+
+
+    }
 
 }
